aefbb0a180bcff1d62bdd505da17190f8ad45f93
Jason Altekruse <altekrusejason@gmail.com>
2022-11-07 21:08:44 -0600

    Revert "WIP - making PHP code work on version 8"
    This reverts commit 0e4361844b2fdf380d3065a8ca6c415448383773.

--- src/Api/jwt.php
+++ src/Api/jwt.php
@@ -10,7 +10,7 @@ include 'db_connection.php';
 use \Firebase\JWT\JWT;
 // require_once "/var/www/html/vendor/autoload.php";
 require_once "vendor/autoload.php";
-$key = new \Firebase\JWT\Key($ini_array['key'], 'HS256');
+$key = $ini_array['key'];
 
 $emailaddress = mysqli_real_escape_string($conn, $_REQUEST['emailaddress']);
 $nineCode = mysqli_real_escape_string($conn, $_REQUEST['nineCode']);
@@ -53,7 +53,7 @@ if ($row['minutes'] > 10) {
             'deviceName' => $deviceName,
             // "expires" => $expirationTime
         ];
-        $jwt = JWT::encode($payload, $key, 'HS256');
+        $jwt = JWT::encode($payload, $key);
 
         $sql = "UPDATE user_device
         SET signedIn = '1'
