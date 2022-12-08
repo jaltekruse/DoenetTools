c06adae4eb2b3b5154ac283ec38cdbb59b61f6a7
Jason Altekruse <altekrusejason@gmail.com>
2022-12-08 16:26:16 -0600

    WIP trying to add LTI depedency

--- src/Api/lti13/login.php
+++ src/Api/lti13/login.php
@@ -1,4 +1,24 @@
 <?php
+use \Packback\Lti1p3\LtiOidcLogin;
+use \Packback\Lti1p3\LtiMessageLaunch;
+use \Packback\Lti1p3\LtiException;
+
+use \Packback\Lti1p3\Interfaces\Database;
+use \Packback\Lti1p3\LtiRegistration;
+use \Packback\Lti1p3\LtiDeployment;
+use \Packback\Lti1p3\OidcException;
+require_once "../vendor/autoload.php";
+
+/*
+class LTI13Database implements Database {
+
+}
+*/
+
+return LtiOidcLogin::new(null, null, null)
+->doOidcLoginRedirect(url("lti13/launch"))
+->doRedirect();
+
 echo "lti 1.3 login"
 
 ?>
