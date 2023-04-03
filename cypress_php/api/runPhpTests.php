0320be50e19aab799ff3710afef512f3df259de2
Jason Altekruse <altekrusejason@gmail.com>
2023-04-03 11:15:46 -0500

    WIP adding php tests
    Start of writing tests in php itself
    
    Adding first PHP test, debugging test failure
    
    why am I getting a different error than when accessing
    this API through the real application?
    
    Problem was I added an error handler that would capture everything
    
    Including "errors" of type "notice", which don't normally stop the
    script, and in my error handler I was echoing information about
    the error/notice to the request output, which I want to be able to
    capture and parse as JSON in most cases.
    
    Overall putting test results into the overall browser requests for
    the "runTests" api, while wanting to capture output of individual
    scripts and assert on them is a bit unergenomic, but I think I might
    have worked out the bulk of the problems, the current test file looks
    pretty clean and the extra stuff is hidden in 'utilities.php' and the runner.
    
    Solution for now, just make the error handler put info in the logs,
    but in the longer run it might be nice to surface this kind of info
    in the test results to make it more obvious.
    
    Utility function for checking a sql table from a test

---
+++ cypress_php/api/runPhpTests.php
@@ -0,0 +1,101 @@
+<?php
+include_once '../api/db_connection.php';
+
+// save this connection under a different name, as other scripts will initialize
+// a db connection under this default name when they include db_connection.php
+$db_conn = $conn;
+
+use \Firebase\JWT\JWT;
+require_once "../api/vendor/autoload.php";
+$key = $ini_array['key'];
+
+include_all_tests(".");
+
+function include_all_tests($folder){
+    foreach (glob("{$folder}/*test*.php") as $filename)
+    {
+        include_once($filename);
+    }
+}
+
+$unit_test_classes = array();
+
+// TODO - this currently isn't working, not sure how to access classes declared in
+// an included script file, going to explicitly run the project specific tests seprately
+// for now
+
+foreach( get_declared_classes() as $class ) {
+    if (in_array('Unit_Tests', class_parents($class))) {
+      $unit_test_classes[] = new $class;
+    }
+}
+
+
+$old_error_handler = set_error_handler("myErrorHandler");
+
+//$unit_test_classes[0]->init_tests();
+
+ob_start( null, 0, PHP_OUTPUT_HANDLER_CLEANABLE | PHP_OUTPUT_HANDLER_REMOVABLE);
+
+$_REQUEST['userId'] = 'cyuserid';
+$payload = [
+    // "email" => $emailaddress,
+    'userId' => 'cyuserid',
+    'deviceName' => "does this matter?",
+    // "expires" => $expirationTime
+];
+$jwt = JWT::encode($payload, $key);
+$_COOKIE['JWT'] = $jwt;
+
+foreach ($unit_test_classes as $unit_tests) {
+    $reflection = new ReflectionClass($unit_tests);
+    try {
+        $unit_tests->init($db_conn);
+    } catch (Exception $ex){
+        echo '<span style="color:red">Error in test init() method</span><br>';
+        echo $ex->getMessage() . "<br>\n";
+    }
+    ob_start( null, 0, PHP_OUTPUT_HANDLER_CLEANABLE | PHP_OUTPUT_HANDLER_REMOVABLE);
+    foreach ($reflection->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
+        if ($method->class == $reflection->getName()) {
+            if ( strpos($method->name, 'test') !== FALSE) {
+                echo "<b>Running test: " . $method->name . "</b><br>\n";
+                error_log("<b>Running test: " . $method->name . "</b><br>\n");
+                $name = $method->name;
+                try {
+                    ob_start( null, 0, PHP_OUTPUT_HANDLER_CLEANABLE | PHP_OUTPUT_HANDLER_REMOVABLE);
+                    $unit_tests->$name();
+                    ob_start( null, 0, PHP_OUTPUT_HANDLER_CLEANABLE | PHP_OUTPUT_HANDLER_REMOVABLE);
+                    echo '<span style="color:blue">SUCCESS</span><br>';
+                    ob_end_flush();
+                } catch (Exception $ex){
+                    echo '<span style="color:red">Test Failure: </span><br>';
+                    echo $ex->getMessage() . "<br>\n";
+                    echo nl2br($ex->getTraceAsString()) . "<br>\n";
+                }
+            }
+        }
+    }
+}
+echo "<h2>All unit tests have been run.</h2><br>\n";
+
+echo "</html>";
+
+// modified version of code available at
+// http://stackoverflow.com/questions/3316899/try-catch-with-php-warnings
+function myErrorHandler($errno, $errstr, $errfile, $errline)
+{
+    $file_parts = explode('/', $errfile);
+    $file_name = $file_parts[count($file_parts) - 1];
+    switch ($errno) {
+        case E_USER_WARNING:    error_log("<b>WARNING</b> [$errno] $errstr<br />\n");  break;
+        case E_NOTICE:          error_log("<b>NOTICE</b> [$errno] $errstr<br />\n");   break;
+        case E_USER_NOTICE:     error_log("<b>USER_NOTICE</b> [$errno] $errstr<br />\n");   break;
+        case E_WARNING:         error_log("Assert failed - " . $file_name . " (" . $errline . ") - " . $errstr . "<br>\n"); break;
+        default:                error_log("Unknown error type: [$errno] in file: " . $file_name . " (" . $errline . ") - " . $errstr . "<br>\n"); break;
+    }
+    /* Do execute PHP internal error handler */
+    return false;
+}
+
+?>
---
+++ cypress_php/api/utilities.php
@@ -0,0 +1,79 @@
+<?php
+
+function assertEquals($expected, $actual, $message = "") {
+  if ($expected !== $actual) {
+      $ret = "";
+      $ret .= "Error: expected value <br>'";
+      $ret .= print_r($expected, TRUE);
+      $ret .= "'<br> but received <br>'";
+      $ret .= print_r($actual, TRUE);
+      $ret .= "'" . ($message != "" ? " - " . $message : "");
+      $ret .= "<br>\n";
+      throw new Exception($ret);
+  }
+}
+
+
+class Unit_Tests {
+  protected $db_conn;
+
+  function init($db_conn) {
+    $this->db_conn = $db_conn;
+  }
+
+  function runScript($scriptFile) {
+    error_log("start runScript");
+    include $scriptFile;
+    error_log("after include script in runScript method");
+    $outputStr;
+    try {
+      $outputStr = $this->getOutputString();
+      $ret = json_decode($outputStr);
+      if (! ($ret && $ret->success)) {
+        throw new Exception("Unexpected error running script " . $scriptFile . ": " . print_r($ret, true));
+      }
+      return $ret;
+    } catch (Throwable $e) {
+        throw new Exception("Unexpected error running script " . $scriptFile . " output not in JSON format: " . $outputStr);
+    }
+  }
+
+  // this assumes you are running a script and expect a parsable JSON result
+  function runScriptExpectError($scriptFile) {
+    include $scriptFile;
+    return json_decode($this->getOutputString());
+  }
+
+  function runScriptExpectUnparsableOutput($scriptFile) {
+    include $scriptFile;
+    return $this->getOutputString();
+  }
+
+  function checkSqlRows($expectedRows, $sql) {
+    $result = $this->db_conn->query($sql);
+    if (!$result) {
+      throw new Exception($this->db_conn->error);
+    }
+    assertEquals($expectedRows, $result->num_rows);
+  }
+
+  function expectOutputString($expected, $message="") {
+    // TODO - hack to get the test output to show in the browser with styles
+    header("Content-Type: text/html");
+    $actual = ob_get_clean();
+    ob_start( null, 0, PHP_OUTPUT_HANDLER_CLEANABLE | PHP_OUTPUT_HANDLER_REMOVABLE);
+
+    assertEquals($expected, $actual, $message);
+  }
+
+  function getOutputString() {
+    // TODO - hack to get the test output to show in the browser with styles
+    header("Content-Type: text/html");
+    $ret = ob_get_clean();
+    //ob_end_clean();
+    ob_start( null, 0, PHP_OUTPUT_HANDLER_CLEANABLE | PHP_OUTPUT_HANDLER_REMOVABLE);
+    return $ret;
+  }
+}
+
+?>
