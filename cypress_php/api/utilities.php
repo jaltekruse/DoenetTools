f0b6fadd9b4f2c421002ced5cee3b312d612b98c
Jason Altekruse <altekrusejason@gmail.com>
2023-04-04 15:24:29 -0500

    Adding first PHP test, debugging test failure
    why am I getting a different error than when accessing
    this API through the real application?

--- cypress_php/api/utilities.php
+++ cypress_php/api/utilities.php
@@ -16,13 +16,42 @@ function assertEquals($expected, $actual, $message = "") {
 
 class Unit_Tests {
 
-  function init_tests() {
+  function init() {
+  }
+
+  function runScript($scriptFile) {
+    error_log("start runScript");
+    include $scriptFile;
+    error_log("after include script in runScript method");
+    $outputStr;
+    try {
+      $outputStr = $this->getOutputString();
+      $ret = json_decode($outputStr);
+      if (! ($ret && $ret->success)) {
+        throw new Exception("Unexpected error running script " . $scriptFile . ": " . print_r($ret, true));
+      }
+      return $ret;
+    } catch (Throwable $e) {
+        throw new Exception("Unexpected error running script " . $scriptFile . " output not in JSON format: " . $outputStr);
+    }
+  }
+
+  // this assumes you are running a script and expect a parsable JSON result
+  function runScriptExpectError($scriptFile) {
+    include $scriptFile;
+    return json_decode($this->getOutputString());
+  }
+
+  function runScriptExpectUnparsableOutput($scriptFile) {
+    include $scriptFile;
+    return $this->getOutputString();
   }
 
   function expectOutputString($expected, $message="") {
     // TODO - hack to get the test output to show in the browser with styles
     header("Content-Type: text/html");
     $actual = ob_get_clean();
+    ob_start( null, 0, PHP_OUTPUT_HANDLER_CLEANABLE | PHP_OUTPUT_HANDLER_REMOVABLE);
 
     assertEquals($expected, $actual, $message);
   }
