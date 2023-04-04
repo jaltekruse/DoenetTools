b3e45e9968aca76bd4740d7935594e4a866aa01c
Jason Altekruse <altekrusejason@gmail.com>
2023-04-04 15:24:29 -0500

    Adding first PHP test, debugging test failure
    why am I getting a different error than when accessing
    this API through the real application?

--- cypress_php/api/testPortfolio.php
+++ cypress_php/api/testPortfolio.php
@@ -5,21 +5,20 @@ class Portfolio_Tests extends Unit_Tests {
 
   private $portfolioCourseId;
 
-  function init_tests() {
-    include "../api/createPortfolioCourse.php";
-    $output = $this->getOutputString();
-    $newlyCreatedCourseId = json_decode($output)->portfolioCourseId;
-    error_log($output);
+  function init() {
+    $_REQUEST["emailaddress"] = "test@test.com";
+    $output = $this->runScript("../api/sendSignInEmail.php");
 
-    include "../api/getPorfolioCourseId.php";
-    $output = $this->getOutputString();
-    error_log(json_decode($output)->portfolioCourseId);
-    $this->portfolioCourseId = json_decode($output)->portfolioCourseId;
+    error_log("start init func");
+    $output = $this->runScript("../api/createPortfolioCourse.php");
+    $newlyCreatedCourseId = $output->portfolioCourseId;
+
+    $this->portfolioCourseId = $this->runScript("../api/getPorfolioCourseId.php")->portfolioCourseId;
     assertEquals($this->portfolioCourseId, $newlyCreatedCourseId);
   }
 
-
-  function test_addition() {
+  function test_createPortfolioActivity() {
+    $output = $this->runScript("../api/createPortfolioActivity.php");
   }
 
   function test_getPortfolioCourseId() {
