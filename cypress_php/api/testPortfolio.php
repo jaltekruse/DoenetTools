fb811421529840e792c9be1076ac024604cea0d4
Jason Altekruse <altekrusejason@gmail.com>
2023-04-05 11:46:21 -0500

    Utility function for checking a sql table from a test

--- cypress_php/api/testPortfolio.php
+++ cypress_php/api/testPortfolio.php
@@ -1,11 +1,13 @@
 <?php
 include_once("utilities.php");
+include "../api/db_connection.php";
 
 class Portfolio_Tests extends Unit_Tests {
 
   private $portfolioCourseId;
 
-  function init() {
+  function init($db_conn) {
+    parent::init($db_conn);
     $_REQUEST["emailaddress"] = "test@test.com";
     $output = $this->runScript("../api/sendSignInEmail.php");
 
@@ -19,13 +21,16 @@ class Portfolio_Tests extends Unit_Tests {
 
   function test_createPortfolioActivity() {
     $output = $this->runScript("../api/createPortfolioActivity.php");
+    assertEquals($output->success, true);
+    $doenetId = $output->doenetId;
+    $this->checkSqlRows(1, "select doenetId from course_content where doenetId = '$doenetId'");
+    
+    $output->pageDoenetId;
   }
 
   function test_getPortfolioCourseId() {
 
   }
-
-
 
 }
 ?>
