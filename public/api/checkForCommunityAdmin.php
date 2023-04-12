67a58ed4875c8fcf65eb163bd9d97f21fa653a35
Jason Altekruse <altekrusejason@gmail.com>
2023-04-12 13:16:13 -0500

    Show admin features to only registered admins

--- public/api/checkForCommunityAdmin.php
+++ public/api/checkForCommunityAdmin.php
@@ -12,36 +12,21 @@ $userId = $jwtArray['userId'];
 
 $response_arr;
 try {
-    if ($userId == '') {
-        throw new Exception('You need to be logged in to do see the promoted materials groups.');
-    } else {
+    $isAdmin = false;
+    if ($userId != '') {
         $sql = 
             "select userId from community_admins
             where userId = '$userId'
             ";
 
         $result = $conn->query($sql);
-        if ($result->num_rows == 0) {
-            throw new Exception('You do not have permissions to modify the promoted materials.');
-        }
-    }
-
-    $sql = 
-        "select groupName, currentlyFeatured, homepage, id
-        from promoted_content_groups pcg";
-
-    $result = $conn->query($sql);
-    if ($result->num_rows <= 0) {
-        throw new Exception("No promoted content groups were found.");
-    } else {
-        $promotedGroups = [];
-        while ($row = $result->fetch_assoc()) {
-            $promotedGroups[] = $row;
+        if ($result->num_rows == 1) {
+            $isAdmin = true;
         }
     }
     $response_arr = [
         'success' => true,
-        'carouselGroups' => $promotedGroups 
+        'isAdmin' => $isAdmin
     ];
     // set response code - 200 OK
     http_response_code(200);
