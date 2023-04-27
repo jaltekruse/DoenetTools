69dbc80eb5e41f773ae33efbddd0b3c6bee04923
Jason Altekruse <altekrusejason@gmail.com>
2023-04-27 12:40:29 -0500

    WIP - fixing stuff

--- public/api/searchPublicActivities.php
+++ public/api/searchPublicActivities.php
@@ -6,61 +6,53 @@ header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 
 require 'defineDBAndUserAndCourseInfo.php';
+include_once './models/baseModel.php';
 
-$success = TRUE;
-$message = "";
+try {
+  Base_Model::checkForRequiredInputs($_REQUEST, ["q"]);
+  $q = mysqli_real_escape_string($conn,$_REQUEST["q"]);
 
-$q = mysqli_real_escape_string($conn,$_REQUEST["q"]);
+  $matchingUsers = [];
+  $matchingActivities = [];
 
-if ($q == ""){
-  $success = FALSE;
-  $message = 'Internal Error: missing search query';
-}
-
-$matchingUsers = [];
-$matchingActivities = [];
-//Get Matching Activities 
-if ($success) {
-
+  //Get Matching Activities 
   // Doesn't show public from courses only portfolios
-  $sql = "
-  SELECT cc.doenetId,
-  CAST(cc.jsonDefinition as CHAR) AS json,
-  cc.imagePath,
-  cc.label,
-  cc.courseId,
-  p.doenetId AS 'pageDoenetId',
-  concat(u.firstName, concat(' ', u.lastName)) as fullName
-  FROM course_content AS cc
-  LEFT JOIN pages AS p
-    ON p.containingDoenetId = cc.doenetId
-  LEFT JOIN course AS c
-    ON c.courseId = cc.courseId
-  join user u on c.portfolioCourseForUserId = u.userId
-  WHERE cc.label LIKE '%$q%'
-  AND cc.isPublic = 1
-  AND cc.isDeleted = 0
-  AND cc.isBanned = 0
-  AND c.portfolioCourseForUserId IS NOT NULL
-  LIMIT 100
-  ";
+  $rows = Base_Model::queryFetchAssoc($conn,
+    "
+    SELECT cc.doenetId,
+    CAST(cc.jsonDefinition as CHAR) AS json,
+    cc.imagePath,
+    cc.label,
+    cc.courseId,
+    p.doenetId AS 'pageDoenetId',
+    concat(u.firstName, concat(' ', u.lastName)) as fullName
+    FROM course_content AS cc
+    LEFT JOIN pages AS p
+      ON p.containingDoenetId = cc.doenetId
+    LEFT JOIN course AS c
+      ON c.courseId = cc.courseId
+    join user u on c.portfolioCourseForUserId = u.userId
+    WHERE cc.label LIKE '%$q%'
+    AND cc.isPublic = 1
+    AND cc.isDeleted = 0
+    AND cc.isBanned = 0
+    AND c.portfolioCourseForUserId IS NOT NULL
+    LIMIT 100
+    ");
   
-  $result = $conn->query($sql); 
-  if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()){
+    foreach ($rows as $row ){
       $json = json_decode($row['json'], true);
-      array_push($matchingActivities,[
-        'type' => 'activity',
-        'doenetId' => $row['doenetId'],
-        'courseId' => $row['courseId'],
-        'version' => $json['version'],
-        'content' => $json['content'],
-        'imagePath' => $row['imagePath'],
-        'label' => $row['label'],
-        'fullName' => $row['fullName'],
-        'public' => '1',
-        'pageDoenetId' => $row['pageDoenetId'],
-      ]);
+      $row['version'] = $json['version'];
+      $row['content'] = $json['content'];
+      array_push(
+        array_merge(
+          $row,
+          [
+            'type' => 'activity',
+            'version' => $json['version'],
+            'content' => $json['content'],
+            'public' => '1',
+          ]));
     }
   }
 
