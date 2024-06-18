649942bd33543266c328de2996d420904d1adc0b
Jason Altekruse <altekrusejason@gmail.com>
2024-06-18 10:58:18 -0500

    Not working yet, still not picking up babel config
    https://github.com/jestjs/jest/issues/8006

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
+console.log(__dirname);
 module.exports = {
-    // Your normal jest config settings
-    testPathIgnorePatterns: ["<rootDir>/cypress/", "<rootDir>/.cache/"],
-    testEnvironment: 'jsdom',
-    transform: {
-        "^.+\\.(js|jsx)$": "babel-jest",
-    },
-    modulePaths: [
-        "<rootDir>"
+  // Your normal jest config settings
+  testPathIgnorePatterns: ["<rootDir>/cypress/", "<rootDir>/.cache/"],
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(js|jsx)$": [
+      "babel-jest",
+      { configFile: path.resolve(__dirname, "babel.config.js") },
     ],
-}
+  },
+  modulePaths: ["<rootDir>"],
+};
